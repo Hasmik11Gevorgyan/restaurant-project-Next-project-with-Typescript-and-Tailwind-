b5b3f0fa9fb95c2e682a063fe2054ea826a80eab
unknown <you@example.com>
2025-11-28 18:54:02 +0400

    ihave done orders Page and fixed my project

--- my-restaurant/src/app/orders/page.tsx
+++ my-restaurant/src/app/orders/page.tsx
@@ -1,9 +1,70 @@
 import React from 'react'
 
-const Orders = () => {
+const OrdersPage = () => {
   return (
-    <div>Orders</div>
+    <div className='p-4 lg:px-20 xl:px-40'>
+      <table className='w-full  border-separate border-spacing-y-3'>
+       <thead>
+       <tr className='text-left'>
+        <th className='hidden md:block'>order Id</th>
+        <th>Date</th>
+        <th>Price</th>
+        <th className='hidden md:block'>Products</th>
+        <th>Status</th>
+        
+       </tr>
+       </thead>
+       <tbody>
+      <tr className='text-sm md:text-base bg-red-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1 text-green-400'>Delivered</td>
+      </tr>
+      <tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr>
+      <tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr><tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr><tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr><tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr><tr className='text-sm md:text-base odd:bg-gray-100'>
+        <td className='hidden md:block py-6 px-1'>#123456</td>
+        <td className='py-6 px-1'>2024-06-15</td>
+        <td className='py-6 px-1'>$45.00</td>
+        <td className='hidden md:block'>Pizza, Salad</td>
+        <td className='py-6 px-1'>Delivered</td>
+      </tr>
+       </tbody>
+      </table>
+      
+      Orders</div>
   )
 }
 
-export default Orders
+export default OrdersPage
